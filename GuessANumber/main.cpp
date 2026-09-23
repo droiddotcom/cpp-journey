aea97520de440f29d230b9d6aa1a2c4576348826
droiddotcom <andreylarch2536@gmail.com>
2026-09-23 21:39:17 +0300

    Добавил режим: играть еще раз

--- GuessANumber/main.cpp
+++ GuessANumber/main.cpp
@@ -8,24 +8,40 @@ int main() {
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-
     srand(time(0));
-    int secret = rand() % 100 + 1;
-    int guess = 0;
-    int attempts = 0;
-    cout << "Угадай число от 1 до 100" << "\n";
-    while (guess != secret)
+    char answer = 'y';
+    
+    
+    
+    
+    while (answer == 'y')
     {
-        cout << "Твоя попытка: ";
-        cin >> guess;
-        attempts++;
-        if (guess > secret) { cout << "Меньше\n\n"; }
-        else if (guess < secret) { cout << "Больше\n\n"; }
+        int secret = rand() % 100 + 1;
+        int attempts = 0;
+        int guess = 0;
+        cout << "Угадай число от 1 до 100\n";
+
+        while (guess != secret) 
+        {
+            cout << "Твое число: ";
+            cin >> guess;
+            attempts++;
+            if (guess > secret) { cout << "Меньше\n\n"; }
+            else if (guess < secret) { cout << "Больше\n\n"; }
+
+        }
+
+        cout << "\nУгадал! Это было число: " << secret << "!" << "\n";
+        cout << "Попыток: " << attempts;
+
+        do
+        {
+            cout << "\nИграть еще? (y/n): \n\n";
+            cin >> answer;
+        } while (answer != 'y' && answer != 'n');
+        system("cls");
 
     }
-    cout << "\nПопыток: " << attempts;
-    cout << "\nУгадал! Это было число: " << secret << "!" << "\n";
-    cin.ignore();
-    cin.get();
+
     return 0;
 }
