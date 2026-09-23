833ab3cc8e639f787c435d44ca494f748f79dd2e
droiddotcom <andreylarch2536@gmail.com>
2026-09-23 18:08:48 +0300

    Тест: обновил email

--- GuessANumber/main.cpp
+++ GuessANumber/main.cpp
@@ -12,15 +12,18 @@ int main() {
     srand(time(0));
     int secret = rand() % 100 + 1;
     int guess = 0;
+    int attempts = 0;
     cout << "Угадай число от 1 до 100" << "\n";
     while (guess != secret)
     {
         cout << "Твоя попытка: ";
         cin >> guess;
+        attempts++;
         if (guess > secret) { cout << "Меньше\n\n"; }
         else if (guess < secret) { cout << "Больше\n\n"; }
 
     }
+    cout << "\nПопыток: " << attempts;
     cout << "\nУгадал! Это было число: " << secret << "!" << "\n";
     cin.ignore();
     cin.get();
