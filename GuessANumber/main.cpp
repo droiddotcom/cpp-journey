1cba7a0b42e8bd359964cb1824d56ce2aa6abeaf
droiddotcom <andreylarch2536@email.com>
2026-09-23 17:52:57 +0300

    Перенес проекты, убрал дубли .gitignore и README.

---
+++ GuessANumber/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+
+
+    srand(time(0));
+    int secret = rand() % 100 + 1;
+    int guess = 0;
+    cout << "Угадай число от 1 до 100" << "\n";
+    while (guess != secret)
+    {
+        cout << "Твоя попытка: ";
+        cin >> guess;
+        if (guess > secret) { cout << "Меньше\n\n"; }
+        else if (guess < secret) { cout << "Больше\n\n"; }
+
+    }
+    cout << "\nУгадал! Это было число: " << secret << "!" << "\n";
+    cin.ignore();
+    cin.get();
+    return 0;
+}
